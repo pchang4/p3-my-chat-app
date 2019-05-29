9ea2a57275a5ad4047643553c40703428f485bcd
pchang4 <43798455+pchang4@users.noreply.github.com>
2019-05-28 21:35:30 -0700

    added chat app

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# p3 my chat app
+# My Chat App
+
+Quick start:
+
+```
+$ npm install
+$ npm run build
+````
+
+## Development
+
+Run Webpack in watch-mode to continually compile the JavaScript as you work:
+
+```
+$ npm run watch
+```
---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/c973dac1-00cf-40af-8d85-d5b628ba28cd/token";
+const instanceLocator = "v1:us1:c973dac1-00cf-40af-8d85-d5b628ba28cd";
+
+export { tokenUrl, instanceLocator }
